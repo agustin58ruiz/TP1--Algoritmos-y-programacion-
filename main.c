0dfa702cda6a5fa1bb7f1ca9a343ab3d5a637309
Agustín Ruiz <50803935+agustin58ruiz@users.noreply.github.com>
2020-10-13 20:59:12 -0300

    Update main.c

--- main.c
+++ main.c
@@ -17,25 +17,25 @@ int main (int argc, char *argv[])
 	status_t st;
 	format_t fmt;
 
-	if((st = validate_arguments(argc, argv, &fmt)) != OK){ 
+	if ((st = validate_arguments(argc, argv, &fmt)) != OK) { 
 		print_error(st);
 		return st;
 	}
 
-	if((file_in=fopen(argv[CMD_ARG_IN_FILE_POS],"rt"))==NULL){
-		st=ERROR_OPEN_NMEA_FILE;
+	if ((file_in = fopen(argv[CMD_ARG_IN_FILE_POS], "rt")) == NULL) {
+		st = ERROR_OPEN_NMEA_FILE;
 		print_error(st);
 		return st;
 	}
 
-	if((file_out=fopen(argv[CMD_ARG_OUT_FLAG_POS+1],"wt"))==NULL){
-		st=ERROR_OPEN_OUT_FILE;
+	if ((file_out = fopen(argv[CMD_ARG_OUT_FLAG_POS+1], "wt")) == NULL) {
+		st = ERROR_OPEN_OUT_FILE;
 		fclose(file_in);
 		print_error(st);
 		return st;
 	}
 
-	if((st=process_export(file_in,file_out,fmt))!=OK){
+	if ((st = process_export(file_in,file_out,fmt)) != OK) {
 		fclose(file_in);
 		fclose(file_out);
 		remove(argv[CMD_ARG_OUT_FLAG_POS+1]);
@@ -44,8 +44,8 @@ int main (int argc, char *argv[])
 	}
 
 	fclose(file_in);
-	if(fclose(file_out)==EOF){
-		st=ERROR_CLOSE_OUT_FILE;
+	if (fclose(file_out) == EOF) {
+		st = ERROR_CLOSE_OUT_FILE;
 		print_error(st);
 		return st;
 	}
